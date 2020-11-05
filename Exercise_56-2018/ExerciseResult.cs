683d01fa157ebc2eeaa87a9fb93498f6fc083ad9
miloslazic1999 <miloslazic1999@gmail.com>
2020-11-05 20:10:00 +0100

    Napravljena klasa ExerciseResult

---
+++ Exercise_56-2018/ExerciseResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_56_2018
+{
+    class ExerciseResult
+    {
+        public ExerciseResult()
+        {
+        }
+
+        public ExerciseResult(int id, string studentName, string studentIndex, int points)
+        {
+            Id = id;
+            StudentName = studentName;
+            StudentIndex = studentIndex;
+            Points = points;
+        }
+
+        public int Id { get; private set; }
+        public string StudentName { get; private set; }
+        public string StudentIndex { get; private set; }
+        public int Points { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {StudentName} {StudentIndex} {Points}";
+        }
+    }
+}
